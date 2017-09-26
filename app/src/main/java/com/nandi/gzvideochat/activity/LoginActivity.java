48721fb5760ee272af577d8a11b706a083ac6a27
baohongyan <657175417@qq.com>
2017-09-26 16:06:14 +0800

    登录页面1

---
+++ app/src/main/java/com/nandi/gzvideochat/activity/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.nandi.gzvideochat.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+import com.nandi.gzvideochat.R;
+
+/**
+ * Created by baohongyan on 2017/9/26.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
