29cd29363d5d22503aad7b56bf3b4009e49b43b6
xingyufeifan <634846539@qq.com>
2017-09-26 11:52:24 +0800

    第一次提交

---
+++ app/src/main/java/com/nandi/gzvideochat/app/MyApplication.java
@@ -0,0 +1,40 @@
+package com.nandi.gzvideochat.app;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+import com.alibaba.sdk.android.push.CloudPushService;
+import com.alibaba.sdk.android.push.CommonCallback;
+import com.alibaba.sdk.android.push.noonesdk.PushServiceFactory;
+
+/**
+ * Created by ChenPeng on 2017/9/26.
+ */
+
+public class MyApplication extends Application {
+    private static final String TAG = "Init";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initCloudChannel(this);
+    }
+    /**
+     * 初始化云推送通道
+     * @param applicationContext
+     */
+    private void initCloudChannel(Context applicationContext) {
+        PushServiceFactory.init(applicationContext);
+        CloudPushService pushService = PushServiceFactory.getCloudPushService();
+        pushService.register(applicationContext, new CommonCallback() {
+            @Override
+            public void onSuccess(String response) {
+                Log.d(TAG, "init cloudchannel success");
+            }
+            @Override
+            public void onFailed(String errorCode, String errorMessage) {
+                Log.d(TAG, "init cloudchannel failed -- errorcode:" + errorCode + " -- errorMessage:" + errorMessage);
+            }
+        });
+    }
+}
